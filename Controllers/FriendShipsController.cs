f1377897ed0fb030cb2208e6db05c575dfbaa95b
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-04-13 13:29:46 -0400

    initial commit

---
+++ Controllers/FriendShipsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UsersManager.Models;
+
+namespace UsersManager.Controllers
+{
+    public class FriendShipsController : Controller
+    {
+        public UsersDBEntities DB = new UsersDBEntities();
+
+        // GET: FriendShips
+        public ActionResult Index()
+        {
+            return View(DB.FriendShips);
+        }
+
+        public bool IsFriendShipStatusUpToDate()
+        {
+            return ((string)Session["FriendShipStatusSerialNumber"] == (string)HttpRuntime.Cache["FriendShipStatusSerialNumber"]);
+        }
+        public void RenewFriendShipStatusSerialNumber()
+        {
+            HttpRuntime.Cache["FriendShipStatusSerialNumber"] = Guid.NewGuid().ToString();
+        }
+
+        public string GetFriendShipStatusSerialNumber()
+        {
+            if (HttpRuntime.Cache["FriendShipStatusSerialNumber"] == null)
+            {
+                RenewFriendShipStatusSerialNumber();
+            }
+            return (string)HttpRuntime.Cache["FriendShipStatusSerialNumber"];
+        }
+
+        public void SetLocalFriendShipStatusSerialNumber()
+        {
+            Session["FriendShipStatusSerialNumber"] = GetFriendShipStatusSerialNumber();
+        }
+
+
+        public PartialViewResult GetFriendShipsStatus(bool forceRefresh=false)
+        {
+            if (forceRefresh || !IsFriendShipStatusUpToDate())
+            {
+                SetLocalFriendShipStatusSerialNumber();
+                return PartialView(UsersDBDAL.FriendShipsStatus(DB, OnlineUsers.CurrentUserId));
+            }
+            return null;
+        }
+        public ActionResult SendFriendShipRequest(int id)
+        {
+            UsersDBDAL.Add_FiendShipRequest(DB, OnlineUsers.CurrentUserId, id);
+            RenewFriendShipStatusSerialNumber();
+            return View();
+        }
+        public ActionResult RemoveFriendShipRequest(int id)
+        {
+            UsersDBDAL.Remove_FiendShipRequest(DB, OnlineUsers.CurrentUserId, id);
+            RenewFriendShipStatusSerialNumber();
+            return View();
+        }
+        public ActionResult AcceptFriendShipRequest(int id)
+        {
+            UsersDBDAL.Accept_FriendShip(DB, OnlineUsers.CurrentUserId, id);
+            RenewFriendShipStatusSerialNumber();
+            return View();
+        }
+        public ActionResult DeclineFriendShipRequest(int id)
+        {
+            UsersDBDAL.Decline_FriendShip(DB, OnlineUsers.CurrentUserId, id);
+            RenewFriendShipStatusSerialNumber();
+            return View();
+        }
+    }
+}
---
+++ Scripts/PartialRefresh.js
@@ -0,0 +1,30 @@
+﻿function installPartialRefresh(serviceURL, divContainerId, refreshRate, callBack = null) {
+    // intallation of partial refresh
+    setInterval(() => { DoPartialRefresh(serviceURL, divContainerId, callBack); }, refreshRate * 1000);
+}
+
+function DoPartialRefresh(serviceURL, divContainerId, callBack = null) {
+    // posts partial refresh
+    $.ajax({
+        url: serviceURL,
+        dataType: "html",
+        success: function (htmlContent) {
+            if (htmlContent !== "") {
+                $("#" + divContainerId).html(htmlContent);
+                if (callBack != null) callBack();
+            }
+        }
+    })
+}
+
+function ajaxActionCall(actionLink) {
+    // Ajax Action Call to actionLink
+    $.ajax({
+        url: actionLink,
+        method: 'GET',
+        success: (data) => {
+            console.log("Result: " + data);
+        }
+    });
+}
+
